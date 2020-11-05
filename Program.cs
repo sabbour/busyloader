db28c34c6ba55c19903f9383a0f3c1aa2c0f31ef
Ahmed Sabbour <asabbour@microsoft.com>
2020-11-04 16:44:31 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace busyloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting calculations");
+            double result=0,baseNumber=24;   
+
+            var sw = new Stopwatch();
+            while(true) {
+              sw=Stopwatch.StartNew();
+              for (var i = Math.Pow(baseNumber, 6); i >= 0; i--) {    
+                result += Math.Atan(i) * Math.Tan(i);
+              };            
+              sw.Stop();
+              Console.WriteLine("Time Taken-->{0}ms",sw.ElapsedMilliseconds);
+            }
+        }
+    }
+}
